de2d6dce003d582728a4ba3ba436bd62aa54f8fc
Charalampos <charalampos.fanoulis@gmail.com>
2018-10-28 19:30:44 +0200

    Add database

---
+++ db.js
@@ -0,0 +1,27 @@
+
+
+module.exports = (sequelize, db) => {
+  Object.defineProperty(db, "users", {
+    value: sequelize.define("users", {
+      active: sequelize.BOOLEAN,
+      username: sequelize.STRING,
+      password: sequelize.TEXT("tiny"),
+      firstName: sequelize.STRING,
+      role: {
+        type: sequelize.ENUM,
+        values: ["user", "admin"],
+      },
+    }),
+  });
+  Object.defineProperty(db, "stores", {
+    value: sequelize.define("stores", {
+      name: sequelize.TEXT("tiny"),
+      serves: {
+        type: sequelize.ENUM,
+        values: ["Σάντουιτς", "Πίτσα", "Ασιατικό", "Burger"],
+      },
+      telephone: sequelize.INTEGER(10),
+      website: sequelize.STRING(128),
+    }),
+  });
+};
